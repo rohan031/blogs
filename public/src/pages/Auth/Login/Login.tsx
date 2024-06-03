653c778bc843c7e13e5671e57f2b474ee9383c99
rohan <rohanverma031@gmail.com>
2024-06-03 12:18:55 +0530

    added login and signup ui

--- public/src/pages/Auth/Login/Login.tsx
+++ public/src/pages/Auth/Login/Login.tsx
@@ -1,8 +1,107 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./login.module.scss";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
-	return <div className={styles.login}>Login</div>;
+	const [userInput, setUserInput] = useState({
+		email: "",
+		password: "",
+	});
+	const [err, setErr] = useState<string | null>(null);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const token = sessionStorage.getItem("token");
+
+		if (token) {
+			navigate("/");
+		}
+	});
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		let key = e.target.name;
+		let value = e.target.value;
+
+		setUserInput((prev) => {
+			return {
+				...prev,
+				[key]: value,
+			};
+		});
+	};
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		fetch("login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				email: userInput.email,
+				password: userInput.password,
+			}),
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				if (res.error) {
+					throw new Error(res.message);
+				}
+
+				const { accessToken } = res.data;
+				sessionStorage.setItem("token", accessToken);
+				navigate("/");
+			})
+			.catch((err) => {
+				console.error(err);
+				setErr(err.message);
+			});
+	};
+
+	return (
+		<div className={styles.login}>
+			<form onSubmit={handleSubmit}>
+				<div className={styles.input}>
+					<label htmlFor="email">Email</label>
+					<input
+						id="email"
+						name="email"
+						type="email"
+						placeholder="email..."
+						value={userInput.email}
+						onChange={handleInputChange}
+						required
+					/>
+				</div>
+
+				<div className={styles.input}>
+					<label htmlFor="password">Password</label>
+					<input
+						id="password"
+						name="password"
+						type="password"
+						placeholder="password..."
+						value={userInput.password}
+						onChange={handleInputChange}
+						required
+					/>
+				</div>
+
+				<div className={styles.button}>
+					<button type="submit">Login</button>
+				</div>
+
+				{err && <p className={styles.error}>{err}</p>}
+
+				<div className={styles.link}>
+					<Link to="/auth/signup">Signup</Link>
+
+					<Link to="/">Home</Link>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default Login;
--- public/src/pages/Auth/Signup/Signup.tsx
+++ public/src/pages/Auth/Signup/Signup.tsx
@@ -1,8 +1,120 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./signup.module.scss";
+import { Link, useNavigate } from "react-router-dom";
 
 function Signup() {
-	return <div className={styles.signup}>Signup</div>;
+	const [userInput, setUserInput] = useState({
+		email: "",
+		password: "",
+		name: "",
+	});
+	const [err, setErr] = useState<string | null>(null);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const token = sessionStorage.getItem("token");
+
+		if (token) {
+			navigate("/auth/login");
+		}
+	});
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		let key = e.target.name;
+		let value = e.target.value;
+
+		setUserInput((prev) => {
+			return {
+				...prev,
+				[key]: value,
+			};
+		});
+	};
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		fetch("signup", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				email: userInput.email,
+				password: userInput.password,
+				name: userInput.name,
+			}),
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				if (res.error) {
+					throw new Error(res.message);
+				}
+
+				navigate("/auth/login");
+			})
+			.catch((err) => {
+				console.error(err);
+				setErr(err.message);
+			});
+	};
+
+	return (
+		<div className={styles.signup}>
+			<form onSubmit={handleSubmit}>
+				<div className={styles.input}>
+					<label htmlFor="name">Name</label>
+					<input
+						id="name"
+						name="name"
+						type="text"
+						placeholder="name..."
+						value={userInput.name}
+						onChange={handleInputChange}
+						required
+					/>
+				</div>
+
+				<div className={styles.input}>
+					<label htmlFor="email">Email</label>
+					<input
+						id="email"
+						name="email"
+						type="email"
+						placeholder="email..."
+						value={userInput.email}
+						onChange={handleInputChange}
+						required
+					/>
+				</div>
+
+				<div className={styles.input}>
+					<label htmlFor="password">Password</label>
+					<input
+						id="password"
+						name="password"
+						type="password"
+						placeholder="password..."
+						value={userInput.password}
+						onChange={handleInputChange}
+						required
+					/>
+				</div>
+
+				<div className={styles.button}>
+					<button type="submit">Signup</button>
+				</div>
+
+				{err && <p className={styles.error}>{err}</p>}
+
+				<div className={styles.link}>
+					<Link to="/auth/login">Login</Link>
+
+					<Link to="/">Home</Link>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default Signup;
